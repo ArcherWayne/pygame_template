b7101782779b4e2ea62616ab0467bd7cde7bb13c
Archer Wayne <archerwayne1216@gmail.com>
2022-08-14 22:32:58 +0800

    update

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import pygame, sys, time
-import setting
+from setting import *
 from debug import debug
 
-# general setup --------------------------------------------------------------------------------------------- #
-## pygame setup
-pygame.init()
-screen = pygame.display.set_mode(setting.window_size)
 
-# pygame.display.set_caption('window_name')
-# pygame.display.set_icon(pygame.image.load('assets/blade game.png'))
-# background_surface = pygame.transform.scale(
-#     pygame.image.load('assets/background/ground.png').convert(), (setting.WIN_WIDTH, setting.WIN_HEIGTH))
-# background_rect = background_surface.get_rect(center=(setting.WIN_WIDTH / 2, setting.WIN_HEIGTH / 2))
-# clock = pygame.time.Clock()
-# font = pygame.font.Font('assets/font/Pixeltype.ttf', 50)
+class MAINGAME:
+    def __init__(self):
+        ## pygame setup
 
-## varibles setup
-game_active = True
-
-# class setup
-# class = Class()
+        pygame.init()
+        self.screen = pygame.display.set_mode(window_size)
+        # pygame.display.set_caption('window_name')
+        # pygame.display.set_icon(pygame.image.load('assets/blade game.png'))
+        # background_surface = pygame.transform.scale(
+        #     pygame.image.load('assets/background/ground.png').convert(), (setting.WIN_WIDTH, setting.WIN_HEIGTH))
+        # background_rect = background_surface.get_rect(center=(setting.WIN_WIDTH / 2, setting.WIN_HEIGTH / 2))
+        # clock = pygame.time.Clock()
+        # font = pygame.font.Font('assets/font/Pixeltype.ttf', 50)
 
 # group setup ----------------------------------------------------------------------------------------------- #
-all_sprites = pygame.sprite.Group()
-collision_sprites = pygame.sprite.Group()
+        self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
-# main ------------------------------------------------------------------------------------------------------ #
-def main():
-    last_time = time.time()
-    while True:
+# class setup
+        # class = Class()
 
+# attribute setup
+        self.last_time = time.time()
+        self.game_active = True
+
+
+    def game_loop(self):
         # delta time    ------------------------------------------------------------------------------------- #
         dt = time.time() - last_time
         last_time = time.time()
@@ -40,15 +40,19 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        if game_active:
-            screen.fill(setting.WHITE)
+        if self.game_active:
+            self.screen.fill(WHITE)
             # screen.blit(background_surface, background_rect)
-            all_sprites.update()
-            all_sprites.draw(screen)
+            self.all_sprites.update()
+            self.all_sprites.draw(self.screen)
 
 
         pygame.display.update()
 
+    def update(self):
+        pass
 
 if __name__ == "__main__":
-    main()
+    main_game = MAINGAME()
+    main_game.game_loop()
+
